34907b59f4b876a2963438bbf711c5ced50a1ae9
kevin <umplujed@hotmail.com>
2024-01-20 09:21:10 -0500

    First commit basic_calc  script: Features C

---
+++ basic_calc.py
@@ -0,0 +1,37 @@
+'''Script title: Basic calculator v1
+Developer kevin benavides
+Date: 18-01-2024'''
+
+'''Description: 
+this calculator let you get the result of 
+4 basic operations drom two numbers
+'''
+
+#INPUTS (two vaiables)
+num1 = 20
+num2 = 3
+
+
+#PROCESS
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#OUTPUTS
+print("::: BASCI CALC V1.0 :::")
+print("number 1: ", num1, type(num1))
+print("number 2: ", num2, type(num2))
+print("the addition is: ", add, type(add))
+print("the subtraction is: ", sub, type(sub))
+print("the multiplication is: ", mul, type(mul))
+print("the division is: ", div, type(div))
+
+print("::: BASCI CALC V1.1 :::")
+print("number 1: ", num1, type(num1))
+print("number 2: ", num2, type(num2))
+print("the addition is: ", num1 + num2, type(add))
+print("the subtraction is: ", num1 - num2, type(sub))
+print("the multiplication is: ", num1 * num2, type(mul))
+print("the division is: ", num1 / num2, type(div))
+
