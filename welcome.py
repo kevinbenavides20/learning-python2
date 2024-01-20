750e4b1c5cf2f809699b59e2603a793e323d01cf
kevin <umplujed@hotmail.com>
2024-01-20 09:08:35 -0500

    add new feature: age calc

--- welcome.py
+++ welcome.py
@@ -1,6 +1,9 @@
 name = "kevin"
 country = "colombia"
+birthday = 1997
+now = 2024
 
 print("You´re ", name, "and you´re from", country)
 print("You´re ", name)
 print("You´re from", country)
+print("You´re age is", now - birthday)
