bd864e0ebc0969dad93b7a50a9788c82a9027141
kevin <umplujed@hotmail.com>
2024-01-20 09:14:15 -0500

    Refactoring code

--- welcome.py
+++ welcome.py
@@ -3,7 +3,5 @@ country = "colombia"
 birthday = 1997
 now = 2024
 
-print("You´re ", name, "and you´re from", country)
-print("You´re ", name)
 print("You´re from", country)
 print("You´re age is", now - birthday)
